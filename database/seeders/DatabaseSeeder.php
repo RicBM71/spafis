1ed8d4d631496a642aa4071ee115208436e87450
RicBM71 <rbartolome@sanaval.com>
2021-02-25 19:00:32 +0100

    desactivo seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,22 +16,22 @@ class DatabaseSeeder extends Seeder
     {
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
-        $this->call(UsersTableSeeder::class);
-        $this->call(GenericasSeeder::class);
-      //  $this->call(BancosTableSeeder::class);
-        $this->call(TratamientosSeeder::class);
-        $this->call(FacultativosSeeder::class);
-        $this->call(MutuasSeeder::class);
-        $this->call(AreasSeeder::class);
-        $this->call(CajasSeeder::class);
-        $this->call(PacientesSeeder::class);
-        $this->call(HistoriasSeeder::class);
-        $this->call(PacbonosSeeder::class);
-        $this->call(AdjuntosSeeder::class);
-        $this->call(CitasSeeder::class);
-        $this->call(BloqueosSeeder::class);
-        $this->call(FacturasSeeder::class);
-        $this->call(MovicuentaSeeder::class);
+    //     $this->call(UsersTableSeeder::class);
+    //     $this->call(GenericasSeeder::class);
+    //   //  $this->call(BancosTableSeeder::class);
+    //     $this->call(TratamientosSeeder::class);
+    //     $this->call(FacultativosSeeder::class);
+    //     $this->call(MutuasSeeder::class);
+    //     $this->call(AreasSeeder::class);
+    //     $this->call(CajasSeeder::class);
+    //     $this->call(PacientesSeeder::class);
+    //     $this->call(HistoriasSeeder::class);
+    //     $this->call(PacbonosSeeder::class);
+    //     $this->call(AdjuntosSeeder::class);
+    //     $this->call(CitasSeeder::class);
+    //     $this->call(BloqueosSeeder::class);
+    //     $this->call(FacturasSeeder::class);
+    //     $this->call(MovicuentaSeeder::class);
 
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
